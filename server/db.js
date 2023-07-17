df5f2a49c35ac459acdf0ee4c06d882bccaa2360
mahdyah <59580413+mahdyah@users.noreply.github.com>
2023-07-17 01:48:45 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 
  const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB,
 })
 
