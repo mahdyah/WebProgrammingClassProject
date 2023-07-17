308fa8bd4ca5709edce347e30e65a106cce4333c
mahdyah <59580413+mahdyah@users.noreply.github.com>
2023-07-17 01:34:31 -0400

    made changes for security

--- server/db.js
+++ server/db.js
@@ -2,7 +2,10 @@ const mysql = require('mysql');
 
 
  const db = mysql.createConnection({
- 
+    host:'homecaredb.clcchrd8h5vh.us-east-2.rds.amazonaws.com',
+    user:'admin',
+    password:'Myhomecare57!',
+    database:'homecaredb'
 })
 
 module.exports = {db}
